0bcfc12af92283aa5e8e4d5b4d5f7f3a85b924a2
tugane <tooamiel@gmail.com>
2022-09-04 16:39:44 +0200

    removing Reminder

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,13 @@ function App() {
     setReminders(reminders);
   };
 
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter((reminder) => reminder.id !== id));
+  };
+
   return (
     <div className="App">
-      <ReminderList items={reminders} />
+      <ReminderList onRemoveReminder={removeReminder} items={reminders} />
     </div>
   );
 }
