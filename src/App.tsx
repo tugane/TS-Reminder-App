297cccdc77736985bc3cc112b9dcf559bbe223e2
tugane <tooamiel@gmail.com>
2022-09-04 16:30:46 +0200

    loading reminders from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/Reminder";
+import reminderService from "./services/reminder";
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    { id: 1, title: "Reminder1" },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
